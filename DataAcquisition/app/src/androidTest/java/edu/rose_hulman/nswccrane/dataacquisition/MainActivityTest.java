adc86dcb842137a7a14415263a3c8510a9dfca3a
Jeremiah Goist <goistjt@rose-hulman.edu>
2016-09-18 16:11:50 -0400

    testSensorLabelsExist passes Added @Before to verify all labels match their expected default values

--- DataAcquisition/app/src/androidTest/java/edu/rose_hulman/nswccrane/dataacquisition/MainActivityTest.java
+++ DataAcquisition/app/src/androidTest/java/edu/rose_hulman/nswccrane/dataacquisition/MainActivityTest.java
@@ -2,6 +2,7 @@ package edu.rose_hulman.nswccrane.dataacquisition;
 
 import android.support.test.runner.AndroidJUnit4;
 
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
@@ -11,10 +12,8 @@ import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
 import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
 
-/**
- * Created by goistjt on 9/17/2016.
- */
 @RunWith(AndroidJUnit4.class)
 public class MainActivityTest extends JUnitTestCase<MainActivity> {
 
@@ -45,6 +44,19 @@ public class MainActivityTest extends JUnitTestCase<MainActivity> {
         onView(withId(R.id.pitch_gyro_text_view)).check(matches(isDisplayed()));
         onView(withId(R.id.yaw_gyro_text_view)).check(matches(isDisplayed()));
         onView(withId(R.id.roll_gyro_text_view)).check(matches(isDisplayed()));
+    }
+
+    @Before
+    public void testDefaultStrings() {
+        onView(withId(R.id.calibration_button)).check(matches(withText(R.string.calibrate)));
+        onView(withId(R.id.collection_button)).check(matches(withText(R.string.collect_data)));
+        onView(withId(R.id.export_button)).check(matches(withText(R.string.export_data)));
+        onView(withId(R.id.x_accel_text_view)).check(matches(withText(R.string.x_accel_default)));
+        onView(withId(R.id.y_accel_text_view)).check(matches(withText(R.string.y_accel_default)));
+        onView(withId(R.id.z_accel_text_view)).check(matches(withText(R.string.z_accel_default)));
+        onView(withId(R.id.pitch_gyro_text_view)).check(matches(withText(R.string.pitch_gyro_default)));
+        onView(withId(R.id.roll_gyro_text_view)).check(matches(withText(R.string.roll_gyro_default)));
+        onView(withId(R.id.yaw_gyro_text_view)).check(matches(withText(R.string.yaw_gyro_default)));
     }
 
 }
