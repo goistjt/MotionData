f3f007f40e94955b40f8479856a02a149815723e
goistjt <goistjt@rose-hulman.edu>
2016-09-17 22:49:00 -0400

    testCalibrationButtonExists passes

--- DataAcquisition/app/src/androidTest/java/edu/rose_hulman/nswccrane/dataacquisition/MainActivityTest.java
+++ DataAcquisition/app/src/androidTest/java/edu/rose_hulman/nswccrane/dataacquisition/MainActivityTest.java
@@ -1,15 +1,26 @@
 package edu.rose_hulman.nswccrane.dataacquisition;
 
+import android.support.test.runner.AndroidJUnit4;
+
 import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import edu.rose_hulman.nswccrane.dataacquisition.internal.JUnitTestCase;
 
 import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
 import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
 
 /**
  * Created by goistjt on 9/17/2016.
  */
-public class MainActivityTest {
+@RunWith(AndroidJUnit4.class)
+public class MainActivityTest extends JUnitTestCase<MainActivity> {
+
+    public MainActivityTest() {
+        super(MainActivity.class);
+    }
 
     @Test
     public void testCalibrationButtonExists() {
