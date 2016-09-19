3c7009b94ddc2d4fe143076c751e6a6b5f76bfec
goistjt <goistjt@rose-hulman.edu>
2016-09-19 16:17:14 -0400

    Forcing the wake lock in code

--- DataAcquisition/app/src/main/java/edu/rose_hulman/nswccrane/dataacquisition/MainActivity.java
+++ DataAcquisition/app/src/main/java/edu/rose_hulman/nswccrane/dataacquisition/MainActivity.java
@@ -1,13 +1,22 @@
 package edu.rose_hulman.nswccrane.dataacquisition;
 
+import android.app.KeyguardManager;
+import android.content.Context;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.WindowManager;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        if (BuildConfig.DEBUG) {
+            KeyguardManager km = (KeyguardManager) getSystemService(Context.KEYGUARD_SERVICE);
+            KeyguardManager.KeyguardLock keyguardLock = km.newKeyguardLock("TAG");
+            keyguardLock.disableKeyguard();
+            getWindow().addFlags(WindowManager.LayoutParams.FLAG_TURN_SCREEN_ON);
+        }
         setContentView(R.layout.activity_main);
     }
 }
