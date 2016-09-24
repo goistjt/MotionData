0a6f892ddbe6607247176982608a8ca5579a9642
Jeremiah Goist <goistjt@rose-hulman.edu>
2016-09-24 18:09:37 -0400

    Calibration button opens dialog with instructions.

--- DataAcquisition/app/src/androidTest/java/edu/rose_hulman/nswccrane/dataacquisition/MainActivityTest.java
+++ DataAcquisition/app/src/androidTest/java/edu/rose_hulman/nswccrane/dataacquisition/MainActivityTest.java
@@ -9,6 +9,7 @@ import org.junit.runner.RunWith;
 import edu.rose_hulman.nswccrane.dataacquisition.internal.JUnitTestCase;
 
 import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
 import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
@@ -19,6 +20,19 @@ public class MainActivityTest extends JUnitTestCase<MainActivity> {
 
     public MainActivityTest() {
         super(MainActivity.class);
+    }
+
+    @Before
+    public void testDefaultStrings() {
+        onView(withId(R.id.calibration_button)).check(matches(withText(R.string.calibrate)));
+        onView(withId(R.id.collection_button)).check(matches(withText(R.string.collect_data)));
+        onView(withId(R.id.export_button)).check(matches(withText(R.string.export_data)));
+        onView(withId(R.id.x_accel_text_view)).check(matches(withText(R.string.x_accel_default)));
+        onView(withId(R.id.y_accel_text_view)).check(matches(withText(R.string.y_accel_default)));
+        onView(withId(R.id.z_accel_text_view)).check(matches(withText(R.string.z_accel_default)));
+        onView(withId(R.id.pitch_gyro_text_view)).check(matches(withText(R.string.pitch_gyro_default)));
+        onView(withId(R.id.roll_gyro_text_view)).check(matches(withText(R.string.roll_gyro_default)));
+        onView(withId(R.id.yaw_gyro_text_view)).check(matches(withText(R.string.yaw_gyro_default)));
     }
 
     @Test
@@ -46,17 +60,12 @@ public class MainActivityTest extends JUnitTestCase<MainActivity> {
         onView(withId(R.id.roll_gyro_text_view)).check(matches(isDisplayed()));
     }
 
-    @Before
-    public void testDefaultStrings() {
-        onView(withId(R.id.calibration_button)).check(matches(withText(R.string.calibrate)));
-        onView(withId(R.id.collection_button)).check(matches(withText(R.string.collect_data)));
-        onView(withId(R.id.export_button)).check(matches(withText(R.string.export_data)));
-        onView(withId(R.id.x_accel_text_view)).check(matches(withText(R.string.x_accel_default)));
-        onView(withId(R.id.y_accel_text_view)).check(matches(withText(R.string.y_accel_default)));
-        onView(withId(R.id.z_accel_text_view)).check(matches(withText(R.string.z_accel_default)));
-        onView(withId(R.id.pitch_gyro_text_view)).check(matches(withText(R.string.pitch_gyro_default)));
-        onView(withId(R.id.roll_gyro_text_view)).check(matches(withText(R.string.roll_gyro_default)));
-        onView(withId(R.id.yaw_gyro_text_view)).check(matches(withText(R.string.yaw_gyro_default)));
+    @Test
+    public void testCalibrationDialog() {
+        onView(withId(R.id.calibration_button)).perform(click());
+        onView(withText(R.string.calibration_dialog_title)).check(matches(isDisplayed()));
+        onView(withText(R.string.calibrate)).check(matches(isDisplayed()));
+        onView(withText(R.string.cancel)).check(matches(isDisplayed()));
     }
 
 }
--- DataAcquisition/app/src/main/java/edu/rose_hulman/nswccrane/dataacquisition/MainActivity.java
+++ DataAcquisition/app/src/main/java/edu/rose_hulman/nswccrane/dataacquisition/MainActivity.java
@@ -2,11 +2,20 @@ package edu.rose_hulman.nswccrane.dataacquisition;
 
 import android.app.KeyguardManager;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.view.WindowManager;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    @BindView(R.id.calibration_button)
+    Button mCalibrationButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,5 +27,37 @@ public class MainActivity extends AppCompatActivity {
             getWindow().addFlags(WindowManager.LayoutParams.FLAG_TURN_SCREEN_ON);
         }
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+        this.mCalibrationButton.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.calibration_button:
+                openCalibrationDialog();
+                break;
+            default:
+                //Empty
+        }
+    }
+
+    private void openCalibrationDialog() {
+        AlertDialog.Builder dialog = new AlertDialog.Builder(this);
+        dialog.setTitle(R.string.calibration_dialog_title);
+        dialog.setMessage("Please secure the device where desired within the vehicle, and turn the engine on before calibration");
+        dialog.setPositiveButton(R.string.calibrate, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                // Nothing yet
+            }
+        });
+        dialog.setNegativeButton(R.string.cancel, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                // Nothing yet
+            }
+        });
+        dialog.show();
     }
 }
