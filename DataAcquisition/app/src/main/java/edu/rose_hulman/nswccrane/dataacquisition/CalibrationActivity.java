b88056682413dccd72dd83a902101c72b6750861
Jeremiah Goist <goistjt@rose-hulman.edu>
2016-09-24 19:29:28 -0400

    Small changes to default calibration screen

--- DataAcquisition/app/src/main/java/edu/rose_hulman/nswccrane/dataacquisition/CalibrationActivity.java
+++ DataAcquisition/app/src/main/java/edu/rose_hulman/nswccrane/dataacquisition/CalibrationActivity.java
@@ -1,16 +1,28 @@
 package edu.rose_hulman.nswccrane.dataacquisition;
 
 import android.os.Bundle;
-import android.os.PersistableBundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.TextView;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 /**
  * Created by Jeremiah Goist on 9/24/2016.
  */
 public class CalibrationActivity extends AppCompatActivity {
+    @BindView(R.id.time_remaining)
+    TextView mTimeRemaining;
+
+    private final int CALIBRATION_TIME = 30;
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_calibration);
+        ButterKnife.bind(this);
+        mTimeRemaining.setText(getString(R.string.time_remaining, CALIBRATION_TIME));
+        mTimeRemaining.setVisibility(View.VISIBLE);
     }
 }
