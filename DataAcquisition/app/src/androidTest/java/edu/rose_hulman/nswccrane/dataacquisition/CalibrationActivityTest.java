5727058ca2b0a0bedf8c7ca89778948920086f62
Jeremiah Goist <goistjt@rose-hulman.edu>
2016-09-26 12:00:40 -0400

    testOnGyro/AccelChange fails. Changing to check maximum discrete difference.

--- DataAcquisition/app/src/androidTest/java/edu/rose_hulman/nswccrane/dataacquisition/CalibrationActivityTest.java
+++ DataAcquisition/app/src/androidTest/java/edu/rose_hulman/nswccrane/dataacquisition/CalibrationActivityTest.java
@@ -34,9 +34,9 @@ public class CalibrationActivityTest extends JUnitTestCase<CalibrationActivity> 
         Assert.assertEquals(1F, mActivity.max_y_noise);
         Assert.assertEquals(1F, mActivity.max_z_noise);
         mActivity.accelerometerChanged(new float[]{2.5F, 2.5F, 2.5F});
-        Assert.assertEquals(2.5F, mActivity.max_x_noise);
-        Assert.assertEquals(2.5F, mActivity.max_y_noise);
-        Assert.assertEquals(2.5F, mActivity.max_z_noise);
+        Assert.assertEquals(2F, mActivity.max_x_noise);
+        Assert.assertEquals(2F, mActivity.max_y_noise);
+        Assert.assertEquals(2F, mActivity.max_z_noise);
     }
 
     @Test
@@ -50,8 +50,8 @@ public class CalibrationActivityTest extends JUnitTestCase<CalibrationActivity> 
         Assert.assertEquals(1F, mActivity.max_pitch_noise);
         Assert.assertEquals(1F, mActivity.max_yaw_noise);
         mActivity.gyroscopeChanged(new float[]{2.5F, 2.5F, 2.5F});
-        Assert.assertEquals(2.5F, mActivity.max_roll_noise);
-        Assert.assertEquals(2.5F, mActivity.max_pitch_noise);
-        Assert.assertEquals(2.5F, mActivity.max_yaw_noise);
+        Assert.assertEquals(2F, mActivity.max_roll_noise);
+        Assert.assertEquals(2F, mActivity.max_pitch_noise);
+        Assert.assertEquals(2F, mActivity.max_yaw_noise);
     }
 }
