ce532db451c460f4089939b6389ae15a0d2a2d5a
brownba <brownba1@rose-hulman.edu>
2016-09-26 23:10:21 -0400

    setting up travis CI and removing deprecation warning on tests

--- flask-server/server_test.py
+++ flask-server/server_test.py
@@ -15,19 +15,19 @@ class FlaskTestCase(unittest.TestCase):
 
     def test_hello_world(self):
         response = self.app.get('/hello-world')
-        self.assertEquals(b'Hello World!', response.data)
+        self.assertEqual(b'Hello World!', response.data)
 
     def test_echo(self):
         test_input = {'usernames': ['test_username']}
         response = self.app.get('/echo?usernames=test_username')
         resp_json = json.loads(response.data.decode("utf-8"))
-        self.assertEquals(test_input, resp_json)
+        self.assertEqual(test_input, resp_json)
 
     def test_db_select_access(self):
         test_input = {'row': '((\'2016\', \'1\', \'1\', \'1\', b\'hear\', \'1\'),)'}
         response = self.app.get('/gyro')
         resp_json = json.loads(response.data.decode("utf-8"))
-        self.assertEquals(test_input, resp_json)
+        self.assertEqual(test_input, resp_json)
 
 
 if __name__ == '__main__':
