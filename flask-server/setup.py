a9f904e89b96a811491a2a2776d813af56ffda8e
brownba <brownba1@rose-hulman.edu>
2016-09-27 10:48:27 -0400

    trying to get CI working properly

--- flask-server/setup.py
+++ flask-server/setup.py
@@ -3,5 +3,5 @@ from distutils.core import setup
 setup(name='InvalidUsage',
       version='1.0',
       py_modules=['ServerException'],
-      package_dir={'': 'flask-server'}
+      packages=['ServerException']
       )
