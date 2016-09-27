d4aaee263a719b41324b3ac2396aded9f2cd6ff9
brownba <brownba1@rose-hulman.edu>
2016-09-27 14:32:50 -0400

    trying to get CI working properly

--- flask-server/FlaskServer.py
+++ flask-server/FlaskServer.py
@@ -1,6 +1,22 @@
 import _mysql
 from flask import Flask, jsonify, request
-from modules.ServerException import *
+
+
+class InvalidUsage(Exception):
+    status_code = 400
+
+    def __init__(self, message, status_code=None, payload=None):
+        Exception.__init__(self)
+        self.message = message
+        if status_code is not None:
+            self.status_code = status_code
+        self.payload = payload
+
+    def to_dict(self):
+        rv = dict(self.payload or ())
+        rv['message'] = self.message
+        return rv
+
 
 app = Flask(__name__)
 db = _mysql.connect(user='root',
--- flask-server/setup.py
+++
@@ -1,7 +0,0 @@
-from distutils.core import setup
-
-setup(name='InvalidUsage',
-      version='1.0',
-      py_modules=['modules', 'ServerException'],
-      packages=['modules.ServerException']
-      )
