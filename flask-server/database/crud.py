b711bb4722acbbfec75d2297bd247136ac104053
RunZGit <yangr@rose-hulman.edu>
2016-10-10 20:37:26 -0400

    config prob

--- flask-server/database/crud.py
+++ flask-server/database/crud.py
@@ -1,5 +1,5 @@
 from mysql.connector import MySQLConnection, Error
-from python_mysql_dbconfig import read_db_config
+from database.python_mysql_dbconfig import read_db_config
 from datetime import datetime
 import hashlib
  
