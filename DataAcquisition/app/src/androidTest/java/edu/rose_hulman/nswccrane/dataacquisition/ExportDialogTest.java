a0a2593128c21693770164b53eecdacaa38402df
trottasn <trottasn@rose-hulman.edu>
2016-10-18 00:46:21 -0400

    yml

--- DataAcquisition/app/src/androidTest/java/edu/rose_hulman/nswccrane/dataacquisition/ExportDialogTest.java
+++ DataAcquisition/app/src/androidTest/java/edu/rose_hulman/nswccrane/dataacquisition/ExportDialogTest.java
@@ -57,8 +57,10 @@ public class ExportDialogTest extends JUnitTestCase<MainActivity> {
     @Test
     public void openCloseNewSessionDialog() throws InterruptedException {
         closeSoftKeyboard();
+        Thread.sleep(1000);
         onView(withId(R.id.new_session_button)).perform(new ClickAction(Tap.SINGLE, GeneralLocation.CENTER, Press.FINGER, null));
-        waitForFragment(NewSessionDialog.TAG, 5000);
+        //waitForFragment(NewSessionDialog.TAG, 5000);
+        Thread.sleep(1000);
         onView(withId(R.id.description_edit_text)).check(matches(isDisplayed()));
         onView(withId(R.id.description_text)).check(matches(withText("Enter a description")));
         onView(withId(R.id.collection_time_selector)).check(matches(isDisplayed()));
@@ -71,7 +73,8 @@ public class ExportDialogTest extends JUnitTestCase<MainActivity> {
         closeSoftKeyboard();
         Thread.sleep(1000);
         onView(withId(R.id.add_to_session_button)).perform(new ClickAction(Tap.SINGLE, GeneralLocation.CENTER, Press.FINGER, null));
-        waitForFragment(AddSessionDialog.TAG, 5000);
+        //waitForFragment(AddSessionDialog.TAG, 5000);
+        Thread.sleep(1000);
         onView(withId(R.id.session_selector)).check(matches(isDisplayed()));
         onView(withId(R.id.collection_time_selector2)).check(matches(isDisplayed()));
         onView(withId(R.id.add_sess_submit_button)).perform(click());
