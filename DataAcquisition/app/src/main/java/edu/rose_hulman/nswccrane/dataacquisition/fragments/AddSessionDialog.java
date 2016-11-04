d275a1792118f0c4afb4d885c1f603fe32443c45
goistjt <goistjt@rose-hulman.edu>
2016-11-04 19:32:00 -0400

    AddSessDialog time selector now shows currently selected time.

--- DataAcquisition/app/src/main/java/edu/rose_hulman/nswccrane/dataacquisition/fragments/AddSessionDialog.java
+++ DataAcquisition/app/src/main/java/edu/rose_hulman/nswccrane/dataacquisition/fragments/AddSessionDialog.java
@@ -3,15 +3,20 @@ package edu.rose_hulman.nswccrane.dataacquisition.fragments;
 import android.app.Activity;
 import android.app.Dialog;
 import android.app.DialogFragment;
-import android.content.Context;
 import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.v7.app.AlertDialog;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ListAdapter;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.List;
+import java.util.Locale;
 
+import datamodels.SessionModel;
 import datamodels.TimeframeDataModel;
 import edu.rose_hulman.nswccrane.dataacquisition.R;
 import edu.rose_hulman.nswccrane.dataacquisition.adapters.TimeframeAdapter;
@@ -23,6 +28,8 @@ import sqlite.MotionCollectionDBHelper;
 public class AddSessionDialog extends DialogFragment implements View.OnClickListener {
     private Activity mRootActivity;
     ListAdapter mListAdapter;
+    private SessionModel motionDataPostBody;
+    private MotionCollectionDBHelper motionDB;
     public static final String TAG = "ADD_SESSION_DIALOG";
 
     @Override
@@ -35,7 +42,7 @@ public class AddSessionDialog extends DialogFragment implements View.OnClickList
     }
 
     private void populateArrayAdapter() {
-        MotionCollectionDBHelper motionDB = new MotionCollectionDBHelper(mRootActivity);
+        motionDB = new MotionCollectionDBHelper(mRootActivity);
         List<TimeframeDataModel> timeData = motionDB.getAllTimeframesBetween(0L,
                 System.currentTimeMillis());
         mListAdapter = new TimeframeAdapter(getActivity(), R.layout.list_item_timespan, timeData);
@@ -54,12 +61,27 @@ public class AddSessionDialog extends DialogFragment implements View.OnClickList
                 break;
             case R.id.collection_time_selector2:
                 populateArrayAdapter();
+                final Button selector = ((Button) v.findViewById(R.id.collection_time_selector2));
                 new AlertDialog.Builder(getActivity())
                         .setTitle("Select a recording period")
                         .setAdapter(mListAdapter, new DialogInterface.OnClickListener() {
                             @Override
                             public void onClick(DialogInterface dialog, int which) {
-                                dialog.dismiss();
+                                TimeframeDataModel timeframe = (TimeframeDataModel) mListAdapter.getItem(which);
+                                motionDataPostBody = motionDB.getAllDataBetween(timeframe.getStartTime(), timeframe.getEndTime());
+                                motionDataPostBody.setStartTime(timeframe.getStartTime());
+                                StringBuilder sBuilder = new StringBuilder();
+
+                                DateFormat dateFormat = new SimpleDateFormat("yyyy-mm-dd hh:mm:ss", Locale.US);
+
+                                Date date = new Date(timeframe.getStartTime());
+                                String time = dateFormat.format(date);
+                                sBuilder.append(String.format("Start: %s\n", time));
+
+                                date = new Date(timeframe.getEndTime());
+                                time = dateFormat.format(date);
+                                sBuilder.append(String.format("End: %s", time));
+                                selector.setText(sBuilder.toString());
                             }
                         }).show();
                 break;
