69b0d4b710c327cd70e8abc91f5e4493317198dd
goistjt <goistjt@rose-hulman.edu>
2016-12-13 22:10:51 -0500

    We have an awesome green CircularSeekBar!

--- DataAcquisition/app/src/main/java/edu/rose_hulman/nswccrane/dataacquisition/fragments/CalibrationDialog.java
+++ DataAcquisition/app/src/main/java/edu/rose_hulman/nswccrane/dataacquisition/fragments/CalibrationDialog.java
@@ -3,10 +3,13 @@ package edu.rose_hulman.nswccrane.dataacquisition.fragments;
 import android.app.Dialog;
 import android.app.DialogFragment;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.support.annotation.VisibleForTesting;
 import android.support.v7.app.AlertDialog;
 import android.view.View;
 
+import devadvance.circularseekbar.CircularSeekBar;
 import edu.rose_hulman.nswccrane.dataacquisition.CalibrationActivity;
 import edu.rose_hulman.nswccrane.dataacquisition.R;
 
@@ -16,6 +19,8 @@ import edu.rose_hulman.nswccrane.dataacquisition.R;
 
 public class CalibrationDialog extends DialogFragment implements View.OnClickListener {
     public static final String TAG = "CALIBRATION_DIALOG";
+
+    CircularSeekBar mSeekBar;
 
     @Override
     public Dialog onCreateDialog(Bundle savedInstanceState) {
@@ -27,6 +32,7 @@ public class CalibrationDialog extends DialogFragment implements View.OnClickLis
     }
 
     private void initUIElements(View v) {
+        mSeekBar = (CircularSeekBar) v.findViewById(R.id.circularSeekBar1);
         v.findViewById(R.id.calibrate_cancel_dialog_button).setOnClickListener(this);
         v.findViewById(R.id.calibrate_accept_dialog_button).setOnClickListener(this);
     }
@@ -35,6 +41,7 @@ public class CalibrationDialog extends DialogFragment implements View.OnClickLis
     public void onClick(View view) {
         switch (view.getId()) {
             case R.id.calibrate_cancel_dialog_button:
+                setYawOffset(calculateYawOffset());
                 this.dismiss();
                 break;
             case R.id.calibrate_accept_dialog_button:
@@ -43,5 +50,17 @@ public class CalibrationDialog extends DialogFragment implements View.OnClickLis
                 startActivity(startCalibrationIntent);
                 this.dismiss();
         }
+    }
+
+    @VisibleForTesting
+    private void setYawOffset(int offset) {
+        SharedPreferences prefs = getActivity().getSharedPreferences(getString(R.string.calibration_prefs), 0);
+        SharedPreferences.Editor editor = prefs.edit();
+        editor.putInt("yaw_offset", offset);
+        editor.apply();
+    }
+
+    private int calculateYawOffset() {
+        return mSeekBar.getProgressAngle();
     }
 }
