817854bfec42a674f6535a1792b1ac77b1c367e4
Jeremiah Goist <goistjt@rose-hulman.edu>
2017-03-22 13:26:33 -0400

    Documented DeletionActivity methods

--- DataAcquisition/app/src/main/java/edu/rose_hulman/nswccrane/dataacquisition/DeletionActivity.java
+++ DataAcquisition/app/src/main/java/edu/rose_hulman/nswccrane/dataacquisition/DeletionActivity.java
@@ -31,28 +31,59 @@ public class DeletionActivity extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_delete);
-
-        motionDB = new MotionCollectionDBHelper(this);
-        timeData = motionDB.getAllTimeframesBetween(0, System.currentTimeMillis());
-        adapter = new TimeframeAdapter(this, R.layout.list_item_timespan, timeData);
         ButterKnife.bind(this);
-        listView.setAdapter(adapter);
+        initializeDependencies();
+        initializeListView();
+    }
 
+    /**
+     * Populates the {@link ListView} with Timeframes from the database, and assigns a listener
+     * which is used to delete data corresponding to various Timeframes
+     */
+    private void initializeListView() {
+        listView.setAdapter(adapter);
         listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> adapterView, View view, int pos, long l) {
                 TimeframeDataModel data = adapter.getItem(pos);
                 if (data != null) {
-                    motionDB.deleteDataBetween(data.getStartTime(), data.getEndTime());
-                    timeData.remove(pos);
-                    Log.d("DATA LIST", "onItemClick: " + pos);
-                    adapter.notifyDataSetChanged();
+                    deleteData(pos, data);
                 }
-                if (timeData.isEmpty()) {
-                    DeletionActivity.this.finish();
-                }
+                autoFinish();
             }
         });
+    }
+
+    /**
+     * Closes the deletion activity when there is no more data to delete
+     */
+    private void autoFinish() {
+        if (timeData.isEmpty()) {
+            this.finish();
+        }
+    }
+
+    /**
+     * Deletes data at the specified position from the database and the {@link ListView}
+     *
+     * @param pos  position of item pressed in the list
+     * @param data data selected to delete
+     */
+    private void deleteData(int pos, TimeframeDataModel data) {
+        //TODO: Display a dialog asking for confirmation before delete
+        motionDB.deleteDataBetween(data.getStartTime(), data.getEndTime());
+        timeData.remove(pos);
+        Log.d("DATA LIST", "onItemClick: " + pos);
+        adapter.notifyDataSetChanged();
+    }
+
+    /**
+     * Initializes the database and populates the list of Timeframes
+     */
+    private void initializeDependencies() {
+        motionDB = new MotionCollectionDBHelper(this);
+        timeData = motionDB.getAllTimeframesBetween(0, System.currentTimeMillis());
+        adapter = new TimeframeAdapter(this, R.layout.list_item_timespan, timeData);
     }
 
     @Override
