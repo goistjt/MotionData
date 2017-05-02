c1fb72ba5ed084ed09c552927ef4406407f41565
trottasn <trottasn@rose-hulman.edu>
2017-05-02 16:36:43 -0400

    Simplified method Sid and I talked about

--- flask_master/data_analysis/kinematics_keeper.py
+++ flask_master/data_analysis/kinematics_keeper.py
@@ -12,7 +12,6 @@ class KinematicsKeeper(object):
     This class represents the state of a particular degree of motion and composes what is needed to determine the next
     valid motions based upon input and specified constraints.
     """
-
     # For usage in determining what a "new acceleration" is defined as in generating state
     VELOCITY = 'VELOCITY'
     ACCELERATION = 'ACCELERATION'
@@ -33,128 +32,98 @@ class KinematicsKeeper(object):
         """
         Adjusts the values of position, velocity, and acceleration for each interval of this degree of motion.
         The user can then query for these values at a particular moment in time.
-
         :param: new_val - an input value, either acceleration, velocity, or position from the previous to generate from
         :param: starting_val_type - acceleration, velocity, or position based on the constants included in this class
-
         :returns: None
-
         """
-
         dc.getcontext().prec = 6
-
         new_val = dc.Decimal(new_val) * self.one
-
-        new_accel = new_val
 
         if starting_val_type == self.VELOCITY:
             new_accel = self._determine_next_acceleration_by_vel(new_val)
-
         elif starting_val_type == self.POSITION:
             new_accel = self._determine_next_acceleration_by_pos(new_val)
+        else:
+            new_accel = new_val
 
-        print(self._curr_accel)
+        bool_res = True
 
-        new_accel = self.check_max_accel(new_accel)
+        bool_res = bool_res and self.check_max_accel(new_accel)
 
-        new_accel = self.check_accel_onset(new_accel)
+        bool_res = bool_res and self.check_accel_onset(new_accel)
 
         new_vel = self._determine_next_velocity(new_accel)
 
-        new_vel = self.check_max_velocity(new_vel)
+        bool_res and self.check_max_velocity(new_vel)
 
         new_pos = self._determine_next_position(new_accel)
 
-        new_pos = self.check_max_neg_position(new_pos)
+        bool_res = bool_res and self.check_max_neg_position(new_pos)
 
-        new_pos = self.check_max_pos_position(new_pos)
+        bool_res = bool_res and self.check_max_pos_position(new_pos)
+
+        if bool_res:
+            self._curr_pos = new_pos
 
         self._curr_vel = new_vel
+
         self._curr_accel = new_accel
-        self._curr_pos = new_pos
 
     def check_max_accel(self, new_accel):
         """
         Determines if the maximum acceleration was reached.
-
         :param: new_accel - the acceleration to check against the maximum allowed
-
         :returns: the acceleration deemed appropriate after checks have been made
         """
-
         definitive_max = self._max_collection.get_max_accel() * dc.Decimal(self.max_buffer_factor)
         if dc.Decimal(abs(new_accel)) > abs(definitive_max):
-            if dc.Decimal(new_accel) == dc.Decimal(0.0):
-                return dc.Decimal(definitive_max)
-            return dc.Decimal((new_accel / abs(new_accel)) * definitive_max)
-        return dc.Decimal(new_accel)
+            return False
+        return True
 
     def check_accel_onset(self, new_accel):
         """
         Determines if the maximum change in acceleration has been reached.
-
         :param: new_accel - the acceleration to check against (with the current acceleration) the maximum allowed change
                             in acceleration
-
         :returns: the acceleration deemed appropriate after checks have been made
         """
-
-        definitive_max = self._max_collection.get_max_accel_diff() * self.max_buffer_factor
+        definitive_max = self._max_collection.get_max_accel_diff() * dc.Decimal(self.max_buffer_factor)
         if definitive_max < dc.Decimal(abs(new_accel - self._curr_accel)):
-            if new_accel - self._curr_accel == dc.Decimal(0.0):
-                new_accel = dc.Decimal(definitive_max + self._curr_accel)
-            else:
-                new_accel = dc.Decimal((((new_accel - self._curr_accel) / dc.Decimal(
-                    abs(new_accel - self._curr_accel))) * definitive_max) + self._curr_accel)
-        return dc.Decimal(new_accel)
+            return False
+        return True
 
     def check_max_velocity(self, new_vel):
         """
         Determines if the maximum velocity has been reached.
-
         :param: new_vel - the velocity to check against the maximum allowed
-
         :returns: the appropriate velocity
         """
-
         definitive_max = self._max_collection.get_max_vel() * self.max_buffer_factor
         if definitive_max < dc.Decimal(abs(new_vel)):
-            if new_vel == dc.Decimal(0.0):
-                new_vel = definitive_max
-            else:
-                new_vel = (new_vel / dc.Decimal(abs(new_vel))) * definitive_max
-        return new_vel
+            return False
+        return True
 
     def check_max_neg_position(self, new_pos):
         """
         Determines if the maximum negative position has been reached.
-
         :param: new_pos - the position to check against the maximum allowed in the negative direction
-
         :returns: the appropriate position
-
         """
-
         definitive_max = self._max_collection.get_max_neg_exc() * self.max_buffer_factor
-        if definitive_max > dc.Decimal(new_pos):
-            new_pos = definitive_max
-        return new_pos
+        if definitive_max > new_pos:
+            return False
+        return True
 
     def check_max_pos_position(self, new_pos):
-
         """
         Determines if the maximum positive position has been reached.
-
         :param: new_pos - the position to check against the maximum allowed in the positive direction
-
         :returns: the appropriate position
-
         """
-
         definitive_max = self._max_collection.get_max_pos_exc() * self.max_buffer_factor
-        if definitive_max < dc.Decimal(new_pos):
-            new_pos = definitive_max
-        return new_pos
+        if definitive_max < new_pos:
+            return False
+        return True
 
     def get_velocity(self):
         return float(self._curr_vel)
@@ -183,22 +152,18 @@ class KinematicsKeeper(object):
     def _determine_next_acceleration_by_pos(self, new_pos):
         """
         Determines the next accleration based upon existing values and a new position.
-
         :param: new_pos - the new position to determine the next acceleration from
-
         :returns: the appropriate new acceleration for further calculations to use
-
         """
 
         return (
-               (((dc.Decimal(new_pos) * self.one) - self._curr_pos) / self._interval) - self._curr_vel) / self._interval
+                   (((dc.Decimal(
+                       new_pos) * self.one) - self._curr_pos) / self._interval) - self._curr_vel) / self._interval
 
     def _determine_next_acceleration_by_vel(self, new_vel):
         """
         Determines the next acceleration based upon existing values and a new velocity.
-
         :param: new_vel - the velocity to determine the new acceleration from
-
         :returns: the appropriate new acceleration for further calculations to use
         """
         return ((dc.Decimal(new_vel) * self.one) - self._curr_vel) / self._interval
@@ -206,13 +171,9 @@ class KinematicsKeeper(object):
     def _determine_next_position(self, new_accel):
         """
         Determines the next position based upon existing values and a new acceleration.
-
         :param: new_accel - the next accel value to generate state from
-
         :returns: the position of the next state
-
         """
-
         return self._curr_pos + (self._curr_vel * self._interval) + (
             dc.Decimal(1 / 2) * self._curr_accel * (self._interval ** dc.Decimal(2))) + (
                    dc.Decimal(1 / 6) * ((new_accel - self._curr_accel) * (self._interval ** dc.Decimal(3))))
@@ -220,12 +181,8 @@ class KinematicsKeeper(object):
     def _determine_next_velocity(self, new_accel):
         """
         Determines the next velocity based upon existing values and a new acceleration.
-
         :param: new_accel - the next accel to generate state from
-
         :returns: the velocity of the next state
-
         """
-
         return self._curr_vel + self._curr_accel * self._interval + dc.Decimal(1 / 2) * (
             (new_accel - self._curr_accel) * (self._interval ** dc.Decimal(2)))
