6c60054ab1464c7c912f26744c0ab057d72b1f84
trottasn <trottasn@rose-hulman.edu>
2017-05-04 12:07:50 -0400

    finished the adjustments for quality of life changes and easier usage post-install

--- AndroidTransferApp/main_app/main_program.py
+++ AndroidTransferApp/main_app/main_program.py
@@ -3,19 +3,96 @@ import shutil
 import subprocess
 import platform
 
+system_name = platform.system()
+base_location = os.path.dirname(os.path.realpath(__file__))
+SLASH = '/'
+if system_name == 'Linux':
+    # TODO: ADD THESE TO THE 'STATIC' FILES, LIKE PROMPT.TXT
+    SLASH = '/'
+    ADB_ENDING = 'adb'
+    ADB_FILE_LOCATION = os.path.dirname(os.path.realpath(__file__)) + SLASH + 'previous_adb_location.txt'
+    REPO_FILE_LOCATION = os.path.dirname(os.path.realpath(__file__)) + SLASH + 'previous_repo_location.txt'
+else:
+    SLASH = '\\'
+    ADB_ENDING = 'adb.exe'
+    ADB_FILE_LOCATION = os.path.dirname(os.path.realpath(__file__)) + SLASH + 'previous_adb_location.txt'
+    REPO_FILE_LOCATION = os.path.dirname(os.path.realpath(__file__)) + SLASH + 'previous_repo_location.txt'
+
 APP_REPO_LOCATION = 'sdcard/Android/data/edu.rose_hulman.nswccrane.dataacquisition/files/records'
 
 
-def get_android_route():
-    """
-    :returns location of the android cache (the directory transferred files will be placed into.
-    """
-    system_name = platform.system()
-    base_location = os.path.dirname(os.path.realpath(__file__))
-    if system_name == 'Linux':
-        return base_location + '/android_files'
-    else:
-        return os.path.dirname(os.path.realpath(__file__)) + '\\android_files'
+def read_from_adb_file():
+    if not os.path.isfile(ADB_FILE_LOCATION):
+        print('Must specify a new adb file location if no previous record of one exists. Not enough parameters')
+        return None
+
+    try:
+        adb_file = open(ADB_FILE_LOCATION, 'r')
+        adb_location = adb_file.readline()
+        adb_file.close()
+        if adb_location == '':
+            print('No adb location on record')
+            return None
+        return adb_location
+    except Exception as e:
+        print('Could not read adb record file because of the following exception:')
+        print(e)
+        return None
+
+
+def read_from_repo_file():
+    if not os.path.isfile(REPO_FILE_LOCATION):
+        print('Must specify a new destination for the transferred files if no record of one exists.',
+              'Not enough parameters.')
+        return None
+
+    try:
+        repo_file = open(REPO_FILE_LOCATION, 'r')
+        repo_location = repo_file.readline()
+        repo_file.close()
+        if repo_location == '':
+            print('No file repository location on record')
+            return None
+        return repo_location
+    except Exception as e:
+        print('Could not read transfer repository record file because of the following exception:')
+        print(e)
+        return None
+
+
+def write_to_adb_file(new_adb_location):
+    if not os.path.isdir(new_adb_location):
+        print('New path to adb location is not a valid directory/path.',
+              'Please retry with a valid path to the adb file')
+        return None
+
+    try:
+        new_adb_location = new_adb_location + SLASH + ADB_ENDING
+        adb_file = open(ADB_FILE_LOCATION, 'w')
+        adb_file.write(new_adb_location)
+        adb_file.close()
+        return new_adb_location
+    except Exception as e:
+        print('Could not read adb record file because of the following exception:')
+        print(e)
+        return None
+
+
+def write_to_repo_file(new_repo_location):
+    if not os.path.isdir(new_repo_location):
+        print('New path to transfer file repository is not a valid directory/path.',
+              'Please retry with a valid path to the directory you wish to transfer files to.')
+        return None
+
+    try:
+        repo_file = open(REPO_FILE_LOCATION, 'w')
+        repo_file.write(new_repo_location)
+        repo_file.close()
+        return new_repo_location
+    except Exception as e:
+        print('Could not read transfer repository record file because of the following exception:')
+        print(e)
+        return None
 
 
 class Command:
@@ -47,30 +124,51 @@ class UpdateAndroidCache(Command):
         parameters will be ignored.
         :returns the success of the transfer action in total
         """
-        if (len(self.params) > 0) and (self.params[0] is not None):
-            adb_location = self.params[0]
+        num_params = len(self.params)
 
+        if num_params == 0:
+            adb_location = read_from_adb_file()
+            if adb_location is None:
+                return False
+            repo_location = read_from_repo_file()
+            if repo_location is None:
+                return False
+
+        elif len(self.params) == 2:
+            if self.params[0] == '-a':
+                adb_location = write_to_adb_file(self.params[1])
+                if adb_location is None:
+                    return False
+                repo_location = read_from_repo_file()
+                if repo_location is None:
+                    return False
+            elif self.params[0] == '-r':
+                repo_location = write_to_repo_file(self.params[1])
+                if repo_location is None:
+                    return False
+                adb_location = read_from_adb_file()
+                if adb_location is None:
+                    return False
+            else:
+                adb_location = write_to_adb_file(self.params[0])
+                if adb_location is None:
+                    return False
+                repo_location = write_to_repo_file(self.params[1])
+                if repo_location is None:
+                    return False
         else:
-            return False
-
-        repository_dir_location = get_android_route()
-
-        if not os.path.exists(get_android_route()):
-            os.makedirs(get_android_route())
-
-        if not os.path.isfile(adb_location):
-            print("Given adb location does not exist.")
+            print("Incorrect number of parameters. Only zero or two parameters expected.")
             return False
 
         cmd = 'pull'
 
         space = ' '
 
-        cmd = adb_location + space + cmd + space + APP_REPO_LOCATION + space + repository_dir_location
+        cmd = adb_location + space + cmd + space + APP_REPO_LOCATION + space + repo_location
 
         try:
             p1 = subprocess.Popen(cmd.split())
-            time_wait = 10
+            time_wait = 60
             if len(self.params) > 1:
                 try:
                     time_wait = int(self.params[1])
@@ -79,11 +177,13 @@ class UpdateAndroidCache(Command):
                     time_wait = 10
             p1.wait(time_wait)
 
-        except subprocess.CalledProcessError:
+        except Exception as e:
             print(
                 "Error using adb to transfer.",
                 "Possibly due to phone not being connected.",
-                "Please check connection and phone access permissions.")
+                "Please check connection and phone access permissions.",
+                "The exact error that occurred is as follows:")
+            print(e)
             return False
 
         return True
@@ -102,13 +202,21 @@ class ClearAndroidCache(Command):
         Uses the existing parameters to recursively delete all contents.
         :returns the success of the deletions in total
         """
-        if os.path.exists(get_android_route()):
-            try:
-                shutil.rmtree(get_android_route())
+        repo_location = read_from_repo_file()
+        if repo_location is None:
+            print('No previous transfer repository location on record. Did not clear.')
+            return False
 
-            except OSError as e:
-                print(e)
-                return False
+        if os.path.exists(repo_location):
+            for curr_path in os.listdir(repo_location):
+                file_path = os.path.join(repo_location, curr_path)
+                try:
+                    if os.path.isfile(file_path):
+                        os.unlink(file_path)
+                    elif os.path.isdir(file_path):
+                        shutil.rmtree(file_path)
+                except Exception as e:
+                    print(e)
 
         return True
 
